f479711ceab0959cda0f64afe17fb7945f05daaf
Fouad Aziz <fouadaziz427@gmail.com>
2023-03-18 20:01:56 +0500

    fixed radio issues

--- js/tasksBLC.js
+++ js/tasksBLC.js
@@ -5,8 +5,15 @@ var userTasks = {
 
 // Map through each Task
 function TaskIltration(array) {
-  array.userTasks.forEach((task) => {
-    var newTextNode = document.getElementById("appendNewChild");
+  var incomplete = array.userTasks.filter((task) => {
+    return task.check === false;
+  });
+  var completed = array.userTasks.filter((task) => {
+    return task.check === true;
+  });
+
+  incomplete.forEach((task) => {
+    var newTextNode = document.getElementsByClassName("incomplete")[0];
     var stringNode = `<div
     data-w-id="f9655d9a-c9ef-4b52-07e0-d4021edf82cf"
     class="command-menu-option"
@@ -59,7 +66,63 @@ function TaskIltration(array) {
         class="delete-icon"
       />
     </div>`;
-    newTextNode.insertAdjacentHTML("afterend", stringNode);
+    newTextNode.insertAdjacentHTML("beforeend", stringNode);
+  });
+  completed.forEach((task) => {
+    var newTextNode = document.getElementsByClassName("completed")[0];
+    var stringNode = `<div
+    data-w-id="f9655d9a-c9ef-4b52-07e0-d4021edf82cf"
+    class="command-menu-option"
+  >
+    <label class="w-checkbox checkbox-field">
+      <div
+      
+      id="task"
+      txtcontent="${task.taskDescription}"
+        class="w-checkbox-input w-checkbox-input--inputType-custom checkbox ${
+          task.check ? "w--redirected-checked" : "checked"
+        }"
+      ></div>
+      <input
+        type="checkbox"
+        id="checkbox-3"
+        name="checkbox-3"
+        data-name="Checkbox 3"
+        style="opacity: 0; position: absolute; z-index: -1"
+      /><span
+
+      style="${task.check ? "text-decoration:line-through ; opacity:0.6" : ""}"
+      
+        class="checkbox-label w-form-label strike"
+        for="checkbox-3"
+        >${task.taskDescription}</span
+      >
+    </label>
+    <div class="div-block" id="cross">
+      <img
+        src="images/x-icon.svg"
+        id ="${task.taskDescription}"
+        loading="lazy"
+        style="
+          -webkit-transform: translate3d(1px, 0, 0)
+            scale3d(1, 1, 1) rotateX(0) rotateY(0)
+            rotateZ(0) skew(0, 0);
+          -moz-transform: translate3d(1px, 0, 0)
+            scale3d(1, 1, 1) rotateX(0) rotateY(0)
+            rotateZ(0) skew(0, 0);
+          -ms-transform: translate3d(1px, 0, 0)
+            scale3d(1, 1, 1) rotateX(0) rotateY(0)
+            rotateZ(0) skew(0, 0);
+          transform: translate3d(1px, 0, 0)
+            scale3d(1, 1, 1) rotateX(0) rotateY(0)
+            rotateZ(0) skew(0, 0);
+        "
+        data-w-id="f9655d9a-c9ef-4b52-07e0-d4021edf82d6"
+        alt=""
+        class="delete-icon"
+      />
+    </div>`;
+    newTextNode.insertAdjacentHTML("beforeend", stringNode);
   });
 }
 
@@ -78,7 +141,7 @@ newTask.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     event.preventDefault();
 
-    var newTextNode = document.getElementsByClassName("taskTodos")[0];
+    var newTextNode = document.getElementsByClassName("incomplete")[0];
     var stringNode = `<div
     data-w-id="f9655d9a-c9ef-4b52-07e0-d4021edf82cf"
     class="command-menu-option"
@@ -88,7 +151,7 @@ newTask.addEventListener("keypress", (event) => {
       id="task"
       txtcontent="${event.target.value}"
         class="w-checkbox-input w-checkbox-input--inputType-custom checkbox ${
-          event.target.check ? "w--redirected-checked" : ""
+          event.target.check ? "w--redirected-checked" : "checked"
         }"
       ></div>
       <input
@@ -133,7 +196,7 @@ newTask.addEventListener("keypress", (event) => {
       />
     </div>`;
 
-    newTextNode.insertAdjacentHTML("beforeend", stringNode);
+    newTextNode.insertAdjacentHTML("afterbegin", stringNode);
     modifieTaskList(event.target.value);
     newTask.value = "";
   }
@@ -191,6 +254,7 @@ document.addEventListener("click", function (e) {
 
       chrome.storage.sync.set({ userTasks: userTasks.TasksList });
 
+      checkbox.classList.remove("checked");
       checkbox.classList.add("w--redirected-checked");
 
       TaskText.style.textDecoration = "line-through";
@@ -198,10 +262,26 @@ document.addEventListener("click", function (e) {
     } else {
       var TaskText = prnt.querySelector("span");
       var checkbox = prnt.querySelector("#task");
+      var incompleteTasks = prnt
+        .closest(".taskTodos")
+        .querySelector(".incomplete");
 
-      checkbox.classList.remove("w--redirected-checked");
+      checkbox.classList.remove("w--redirected-checked"); /*clean this up*/
+      checkbox.classList.add("checked");
       TaskText.style.textDecoration = "none";
       TaskText.style.opacity = "1";
+
+      var tempUser = userTasks.TasksList[index];
+
+      userTasks.TasksList.splice(index, 1);
+
+      chrome.storage.sync.set({
+        userTasks: [tempUser, ...userTasks.TasksList.reverse()],
+      });
+
+      var tempElement = prnt.closest(".command-menu-option");
+      prnt.closest(".command-menu-option").remove();
+      incompleteTasks.appendChild(tempElement);
     }
   }
 });
